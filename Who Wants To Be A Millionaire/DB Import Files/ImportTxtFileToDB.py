f33d3ce01ace7361be06491b0c9de6a6b6222b52
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2020-01-02 15:58:07 +0000

    Question Table Populated

--- Who Wants To Be A Millionaire/DB Import Files/ImportTxtFileToDB.py
+++ Who Wants To Be A Millionaire/DB Import Files/ImportTxtFileToDB.py
@@ -1,4 +1,3 @@
-import csv
 import pyodbc
 
 QuestionBank = []
@@ -22,20 +21,12 @@ with open("Questions.txt") as question_txt_file:
             QuestionBank.append(Question)
             Question = []
 
-
-
-
-
-
 conn = pyodbc.connect(
     r'Driver={MIcrosoft Access Driver (*.mdb, *.accdb)};'
     r'DBQ=D:\\Who Wants To Be A Millionaire\\Who Wants To Be A Millionaire\\Who Wants To Be A Millionaire Database.accdb;'
-
 )
-conn.setencoding('utf-8')
+
 cursor = conn.cursor()
-
-
 
 for i in range(len(QuestionBank)):
     Question = QuestionBank[i][0]
@@ -46,15 +37,5 @@ for i in range(len(QuestionBank)):
     Answer = QuestionBank[i][5]
     cursor.execute("INSERT INTO Question (ID, Question, OptionA, OptionB, OptionC, OptionD, Answer) VALUES(?,?,?,?,?,?,?)", str(i), Question, OptionA, OptionB, OptionC, OptionD, Answer)
     conn.commit()
-cursor.execute('select * from Question')
-   
-for row in cursor.fetchall():
-    print (row)
 
-# print(QuestionBank[2])
 
-# with open('Questions.csv', 'w', newline = '') as question_csv_file:
-#     question_writer = csv.writer(question_csv_file)
-
-#     for Question in QuestionBank:
-#         question_writer.writerow(Question)
